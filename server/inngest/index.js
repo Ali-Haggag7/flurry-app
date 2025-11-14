2d4d333ddb9426bd148a642f26b4228092f9de13
Ali Haggag <ali.haggag2005@gmail.com>
2025-11-14 23:40:51 +0200

    fix line 6 (sendEmail) inside inngest/index

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -3,7 +3,7 @@ import User from "../models/User.js";
 import Connection from "../models/Connection.js";
 import Story from "../models/Story.js";
 import Message from "../models/Message.js";
-import { sendEmail } from "../utils/sendEmail.js";
+import sendEmail from "../utils/sendEmail.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "my-app" });
